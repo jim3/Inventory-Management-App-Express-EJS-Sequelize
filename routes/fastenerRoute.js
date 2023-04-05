fdb00bd12e3b440da83b3fac96eb27bcb9850404
jim3 <j3.casey@protonmail.com>
2023-04-04 22:06:41 -0400

    Update fastenerRoute.js

--- routes/fastenerRoute.js
+++ routes/fastenerRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/fastenerModel");
-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 router.get("/", (req, res) => {
