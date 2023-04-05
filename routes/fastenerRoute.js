84300d6782c1a1bc48d951635aa3d97710f071d6
jim3 <j3.casey@protonmail.com>
2023-04-05 17:32:46 -0400

    Add files via upload

--- routes/fastenerRoute.js
+++ routes/fastenerRoute.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/fastenerModel");
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
 router.get("/", (req, res) => {
     res.render("index");
 });
 
 router.post("/", (req, res) => {
+    console.log("request body log ----> ", req.body);
     const { partname, quantity, price, ...product } = req.body;
     const productType = Object.keys(product)[0];
-    const productValue = product[productType]; 
+    const productValue = product[productType];
+
     const responseObj = {
         partName: partname,
         partType: productValue,
         quantity,
         price,
     };
-    // send the object to the sqlite3 database via Sequelize ORM
+
     db.Parts.create(responseObj);
 
-    // render back to the index page
     res.render("index", {
         ...responseObj,
     });
 });
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
 module.exports = router;
+
+/* 
+
+{
+    partname: 'bolts',     
+    bolttype: 'square',    
+    quantity: '65',        
+    price: '0.13'
+  }
+
+ */
