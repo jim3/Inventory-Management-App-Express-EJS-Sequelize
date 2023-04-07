e9ec3bda45d376adfb75360279e932cc4633b5f4
jim3 <j3.casey@protonmail.com>
2023-04-07 19:11:48 -0400

    Update fastenerRoute.js

--- routes/fastenerRoute.js
+++ routes/fastenerRoute.js
@@ -2,30 +2,58 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/fastenerModel");
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
-
 router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.post("/", (req, res) => {
-    const { partname, quantity, price, ...product } = req.body;
-    const productType = Object.keys(product)[0];
-    const productValue = product[productType];
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
-    const responseObj = {
-        partName: partname,
-        partType: productValue,
-        quantity,
-        price,
-    };
-
-    db.Parts.create(responseObj);
-
-    res.render("index", {
-        ...responseObj,
-    });
+router.get("/parts", async (req, res) => {
+    try {
+        const parts = await db.Parts.findAll();
+        res.json(parts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
-module.exports = router;
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
+router.get("/parts/:id", async (req, res) => {
+    const part = await db.Parts.findByPk(req.params.id);
+    if (!part) {
+        return res.status(404).json({ error: "Part not found" });
+    }
+    res.json(part);
+});
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+// Create a part in inventory
+router.post("/parts", async (req, res) => {
+    try {
+        const { partname, quantity, price, ...product } = req.body;
+        const productType = Object.keys(product)[0];
+        const productValue = product[productType];
+
+        const responseObj = {
+            partName: partname,
+            partType: productValue,
+            quantity,
+            price,
+        };
+
+        let result = await db.Parts.create(responseObj);
+
+        res.render("index", {
+            ...responseObj,
+        });
+    } catch (err) {
+        console.error();
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+
+module.exports = router;
