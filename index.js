dd5674af5fc6721cea7394ad232c13bb5dc0c4a4
jim3 <j3.casey@protonmail.com>
2023-04-07 22:23:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +13,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + "/public"));
 
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=-
-// Routes
+// routes
 app.use("/", require("./routes/fastenerRoute"));
-
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=-
 
 // Sync the database
 const db = require("./models/fastenerModel");
